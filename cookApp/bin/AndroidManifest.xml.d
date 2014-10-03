9835687b60d2fb21cb6e45bdffa0ff134fc604b2
ccimrie <imrieimrie@gmail.com>
2014-10-03 17:57:58 +0100

    Removed redudant folder.

--- cookApp/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/cimrie/apps/melanoma-project/Melanoma/moleTracker/bin/AndroidManifest.xml : \
-/home/cimrie/apps/melanoma-project/Melanoma/moleTracker/AndroidManifest.xml \
--- cookApp/bin/moleTracker-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/cimrie/apps/melanoma-project/Melanoma/moleTracker/bin/moleTracker-debug-unaligned.apk : \
-/home/cimrie/apps/melanoma-project/Melanoma/moleTracker/bin/moleTracker.ap_ \
-/home/cimrie/apps/melanoma-project/Melanoma/moleTracker/bin/classes.dex \
